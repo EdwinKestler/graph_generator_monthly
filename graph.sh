dfc5161e4eb5ab3be69e1ad1d5c6a16674d1bd3c
PeterArgueta <peterarguedo@gmail.com>
2023-05-14 21:15:35 -0600

    upgrade

--- graph.sh
+++ graph.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 directory="/home/charmeleon/Documents/INSIVUMEH/git/graph_generator_monthly"
-fecha=date
+fecha=$date
 
-#cd $directory && python $directory/downloadwithlink.py && python $directory/dataset.py && $directory/dataset2.py
-
+#cd $directory && python $directory/downloadwithlink.py && python $directory/dataset.py && python $directory/dataset2.py &&
 cd $directory && git add . && 
-git commit -m "$fecha" && git push origin main
+git commit -m "upgrade" && git push origin main
 
