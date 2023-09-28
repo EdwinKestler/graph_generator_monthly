63ff2932089e133ebb28e8f2133b8d664e83a9b0
Edwin Kestler <kestler@flatbox.co>
2023-09-28 17:55:38 -0600

    new commit

--- dataset_modified_multythread.py
+++ dataset_modified_multythread.py
@@ -3,13 +3,51 @@ import os
 import pandas as pd
 from PyQt6.QtWidgets import (QApplication, QLabel, QVBoxLayout, QWidget, QPushButton, QFileDialog, QLineEdit, QProgressBar)
 from PyQt6.QtCore import QThread, pyqtSignal
+from PyQt6.QtCore import Qt
 from bokeh.plotting import output_file, save
-from bokeh.models import Range1d, LinearAxis
+from bokeh.models import Range1d, LinearAxis, HoverTool
 from bokeh.plotting import figure
 import matplotlib.pyplot as plt
-from bokeh.models import HoverTool
+from PyQt6.QtGui import QPixmap
+from PyQt6.QtWidgets import QMessageBox
+from PyQt6.QtWidgets import QHBoxLayout
+import subprocess
+import requests
 
+#get_confirm_token, save_response_content, download_file_from_google_drive
+def get_confirm_token(response):
+    for key, value in response.cookies.items():
+        if key.startswith('download_warning'):
+            return value
 
+    return None
+
+#save_response_content
+def save_response_content(response, destination):
+    CHUNK_SIZE = 32768
+
+    with open(destination, "wb") as f:
+        for chunk in response.iter_content(CHUNK_SIZE):
+            if chunk: # filter out keep-alive new chunks
+                f.write(chunk)
+                
+#download_file_from_google_drive
+def download_file_from_google_drive(file_id, destination):
+    URL = "https://drive.google.com/uc?export=download"
+    
+    session = requests.Session()
+    response = session.get(URL, params = { 'id' : file_id }, stream = True)
+    token = get_confirm_token(response)
+    
+    if token:
+        params = { 'id' : file_id, 'confirm' : token }
+        response = session.get(URL, params = params, stream = True)
+    
+    save_response_content(response, destination)
+    
+def resource_path(relative_path):
+    base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
+    return os.path.join(base_path, relative_path)
 
 class Worker(QThread):
     progress_signal = pyqtSignal(str)
@@ -114,31 +152,73 @@ class WeatherGraphsApp(QWidget):
         self.init_ui()
 
     def init_ui(self):
+        self.setWindowTitle("Generador de Graficas Mensual")
+        main_layout= layout = QVBoxLayout()
+        
+        # Horizontal layout for logos
+        logo_layout = QHBoxLayout()
+        # Logos at the top
+        self.logo1 = QLabel(self)
+        pixmap1 = QPixmap("images\\logo_insivumeh.png")  # replace with your logo's path
+        self.logo1.setPixmap(pixmap1)
+        resized_pixmap1 = pixmap1.scaled(128, 128, Qt.AspectRatioMode.KeepAspectRatio)
+        self.logo1.setPixmap(resized_pixmap1)
+        logo_layout.addWidget(self.logo1)
+        
+        self.logo2 = QLabel(self)
+        pixmap2 = QPixmap("images\\waterco-logo.png")  # replace with your logo's path
+        self.logo2.setPixmap(pixmap2)
+        resized_pixmap2 = pixmap2.scaled(128, 128, Qt.AspectRatioMode.KeepAspectRatio)
+        self.logo2.setPixmap(resized_pixmap2)
+        logo_layout.addWidget(self.logo2)
+        
+        self.logo3 = QLabel(self)
+        pixmap3 = QPixmap("images\\IUCN_logo.png")  # replace with your logo's path
+        self.logo3.setPixmap(pixmap3)
+        resized_pixmap3 = pixmap3.scaled(128, 128, Qt.AspectRatioMode.KeepAspectRatio)
+        self.logo3.setPixmap(resized_pixmap3)
+        logo_layout.addWidget(self.logo3)
+        
+        # Add logos_layout to main layout
+        main_layout.addLayout(logo_layout)
+        
+        # Main layout    
+        self.setLayout(layout)
         self.setWindowTitle("Weather Graphs Generator")
-        layout = QVBoxLayout()
+        
+        # Download Button
+        self.download_button = QPushButton("Download Database")
+        self.download_button.clicked.connect(self.download_database)
+        layout.addWidget(self.download_button)
 
         # Directory Input
-        self.directory_label = QLabel("Output Directory:")
+        self.directory_label = QLabel("Fichero de salida:")
         layout.addWidget(self.directory_label)
         self.directory_edit = QLineEdit()
         layout.addWidget(self.directory_edit)
-        self.directory_browse_button = QPushButton("Browse Directory")
+        self.directory_browse_button = QPushButton("1.Seleccionar Diretorio")
         self.directory_browse_button.clicked.connect(self.browse_directory)
         layout.addWidget(self.directory_browse_button)
 
         # CSV File Input
-        self.csv_label = QLabel("Database CSV:")
+        self.csv_label = QLabel("Base de datos .CSV:")
         layout.addWidget(self.csv_label)
         self.csv_edit = QLineEdit()
         layout.addWidget(self.csv_edit)
-        self.csv_browse_button = QPushButton("Browse CSV")
+        self.csv_browse_button = QPushButton("2.selecionar Archivo .CSV")
         self.csv_browse_button.clicked.connect(self.browse_csv)
         layout.addWidget(self.csv_browse_button)
 
         # Generate Graphs Button
-        self.generate_button = QPushButton("Generate Graphs")
+        self.generate_button = QPushButton("3.Generar Graficos")
         self.generate_button.clicked.connect(self.generate_graphs)
         layout.addWidget(self.generate_button)
+        
+        #Explore button after generating graphs
+        self.explore_button = QPushButton("Explorar archivos generados")
+        self.explore_button.setEnabled(False)  # Initially disable the button
+        self.explore_button.clicked.connect(self.open_output_directory)
+        layout.addWidget(self.explore_button)
 
         # Progress Bar
         self.progress = QProgressBar(self)
@@ -147,29 +227,75 @@ class WeatherGraphsApp(QWidget):
         # Status Updates
         self.status_label = QLabel("")
         layout.addWidget(self.status_label)
+        
+        # About Button
+        self.about_button = QPushButton("Acerca de")
+        self.about_button.clicked.connect(self.show_about)
+        layout.addWidget(self.about_button)
+        
+        
+        
+        # Set the main layout to the widget
+        self.setLayout(main_layout)
+        
+    # Function to download the database
+    def download_database(self):
+        file_id = '19gcM1e5rb-HvJ-MVhNSZgsinNhN0S79Y'
+        destination = 'database.csv'
+        download_file_from_google_drive(file_id, destination)
+        # Optionally, you can add a notification to the user when the download completes
+        print("Database downloaded successfully!")
+        
+    # Function to show "About" dialog
+    def show_about(self):
+        about_message = """
+        Generador de Gráficos Climáticos
 
-        self.setLayout(layout)
+        Esta aplicación ha sido desarrollada bajo la Licencia de Código Abierto MIT.
 
+        Se concede permiso, de forma gratuita, a cualquier persona que obtenga una copia de este software y de los archivos de documentación asociados (el "Software"), para utilizar el Software sin restricciones, incluyendo sin limitación los derechos a usar, copiar, modificar, fusionar, publicar, distribuir, sublicenciar y/o vender copias del Software, y permitir que las personas a las que se les proporcione el Software hagan lo mismo, sujeto a las siguientes condiciones:
+
+        La notificación de derechos de autor anterior y esta notificación de permiso deberán ser incluidas en todas las copias o partes sustanciales del Software.
+
+        EL SOFTWARE SE PROPORCIONA "TAL CUAL", SIN GARANTÍA DE NINGÚN TIPO, EXPRESA O IMPLÍCITA, INCLUYENDO, PERO NO LIMITADO A, LAS GARANTÍAS DE COMERCIABILIDAD, IDONEIDAD PARA UN PROPÓSITO PARTICULAR Y NO INFRACCIÓN. EN NINGÚN CASO LOS AUTORES O TITULARES DE LOS DERECHOS DE AUTOR SERÁN RESPONSABLES DE CUALQUIER RECLAMACIÓN, DAÑOS U OTRA RESPONSABILIDAD, YA SEA EN UNA ACCIÓN DE CONTRATO, AGRAVIO O DE OTRO TIPO, DERIVADA DE, FUERA DE O EN CONEXIÓN CON EL SOFTWARE O SU USO U OTRO TIPO DE ACCIONES EN EL SOFTWARE.
+        """
+        QMessageBox.about(self, "Acerca del Generador de Gráficos Climáticos", about_message)
+        
+    # Function to open the output directory            
     def browse_directory(self):
         directory = QFileDialog.getExistingDirectory(self, "Select Output Directory")
         if directory:
             self.directory_edit.setText(directory)
 
+    # Function to browse the CSV file
     def browse_csv(self):
         file_filter = "CSV Files (*.csv);;All Files (*)"
         file_name, _ = QFileDialog.getOpenFileName(self, "Select Database CSV", "", file_filter)
         if file_name:
             self.csv_edit.setText(file_name)
-    
+            
+    #funciton to open the output directory
+    def open_output_directory(self):
+        output_directory = self.directory_edit.text()
+        if output_directory:
+            # Depending on your OS, the approach to open a folder might differ
+            if sys.platform == "win32":
+                os.startfile(output_directory)
+            elif sys.platform == "darwin":  # macOS
+                subprocess.check_call(["open", output_directory])
+            else:  # linux variants
+                subprocess.check_call(["xdg-open", output_directory])    
+            
+    #Fucntion to generate graphs
     def generate_graphs(self):
         output_directory = self.directory_edit.text()
         csv_file_path = self.csv_edit.text()
 
         if not (output_directory and csv_file_path):
-            self.status_label.setText("Please provide all inputs before generating graphs!")
+            self.status_label.setText("Porfavor provea todos los requisitos para genrar las graficas!")
             return
 
-        self.status_label.setText("Processing... Please wait.")
+        self.status_label.setText("Procesando datos... porfavor espere.")
         directory_img = os.path.join(output_directory, "img_output")
         directory_html = os.path.join(output_directory, "html_output")
         os.makedirs(directory_img, exist_ok=True)
@@ -188,11 +314,17 @@ class WeatherGraphsApp(QWidget):
             self.worker = Worker(grouped_data, directory_img, directory_html)
             self.worker.plot_data_signal.connect(self.plot_with_matplotlib)
             self.worker.progress_signal.connect(self.update_progress)
-            self.worker.finished.connect(lambda: self.status_label.setText("Graphs generated successfully!"))
+            self.worker.finished.connect(lambda: self.status_label.setText("Graficos generados exitosamente!"))
+            self.worker.finished.connect(self.graphs_generated)
+               
             self.worker.start()
 
         except Exception as e:
-            self.status_label.setText(f"Error occurred: {str(e)}")
+            self.status_label.setText(f"Error: {str(e)}")
+            
+    def graphs_generated(self):
+                self.status_label.setText("Graficos generados exitosamente!")
+                self.explore_button.setEnabled(True)
 
     def plot_with_matplotlib(self, data):
         # 1. Extract the data
@@ -240,7 +372,7 @@ class WeatherGraphsApp(QWidget):
         
     def update_progress(self, estacion):
             self.progress.setValue(self.progress.value() + 1)
-            self.status_label.setText(f"Processing {estacion}...")
+            self.status_label.setText(f"Procesando estacion {estacion}...")
 
     
 def main():
